eb607e72b572a6fc88c0115353d0135c59a32f0c
SinghalSahab <prakharsinghal10a@gmail.com>
2025-04-09 23:55:30 +0530

    c

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,1 +1,29 @@
 
+'use client'
+import { currentUser} from "@clerk/nextjs/server";
+import {RedirectToSignIn} from '@clerk/nextjs'
+import { db } from "./db";
+
+export const initialProfile = async () => {
+    const user = await currentUser();
+    if (!user) {
+        return <RedirectToSignIn />;
+    }
+    const profile = await db.profile.findUnique({
+        where: {
+            id: user.id,
+        },
+    });
+    if(profile)
+        return profile;
+    
+    const newProfile = await db.profile.create({
+        data: {
+            userId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            email: user.emailAddresses[0].emailAddress,
+            imageUrk: user.imageUrl,
+        },
+    });
+    return newProfile;
+};
