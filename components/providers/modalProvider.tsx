8ef21c07ee1d505eb9b966267921ad775af5633d
SinghalSahab <prakharsinghal10a@gmail.com>
2025-04-21 03:57:50 +0530

    create Server done

--- components/providers/modalProvider.tsx
+++ components/providers/modalProvider.tsx
@@ -5,6 +5,7 @@ import { InviteModal } from "../modals/inviteModal";
 import { useEffect, useState } from "react";
 import { EditServerModal } from "../modals/editServerModal";
 import { MembersModal } from "../modals/membersModal";
+import { CreateChannelModal } from "../modals/createChannelModel";
 
 const ModalProvider = () => {
   const [isMounted,setIsMounted] = useState(false);
@@ -21,6 +22,7 @@ const ModalProvider = () => {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   );
 };
